ddde7e58d8d5ce04f37d410da4c25a3ecb757b02
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-04 11:50:44 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random
 
-commentsDict = ["good","amazing one","keep going","excellent","next video please","sub to your channel","shared to others","made my day","keep it up","sensational","rock it","challenge it","post video daily","work was amazing","needed more edit","edit was awesome",
-"what a video man","watched yesterday","your are genious","faster than light","your work needed success","marvelous","new fan of you","keep rock dude","copy cat","link the video","listening","writing","reading","playing","eating",] #replace with your words
+commentsDict = ['good','amazing one','keep going','excellent','next video please','sub to your channel','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
+'what a video man','watched yesterday','your are genious','faster than light','your work needed success','new fan of you','keep rock dude','copy cat','link the video','listening','writing','reading','playing',] #replace with your words
 
 email = 'virat@123@gmail.com\n'   #replace with your mail         
 password = 'pass123$%\n'           #replace with your password     
